f185d45718f5f0272da800b74d9277bac649bb98
kanyutu <edwinkanyutu@gmail.com>
2014-02-01 14:39:27 +0300

    Demonstrate git use

--- views.py
+++ views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 def file_not_found_404(request):
-    page_title = 'Page Not Found'
+    page_title = 'Page Not Found,Jipe Shughuli'
     return render_to_response("404.html", locals(), context_instance=RequestContext(request))
 
 def server_error_500(request):
